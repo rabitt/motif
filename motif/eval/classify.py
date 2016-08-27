ffb08594cf1ce8788cb1d8587a4b62a544409953
Rachel Bittner <rachel.bittner@nyu.edu>
2016-08-27 15:40:22 -0400

    add score method

--- motif/eval/classify.py
+++ motif/eval/classify.py
@@ -3,64 +3,54 @@
 """
 from sklearn import metrics
 
-def clf_metrics(p_train, p_test, y_train, y_test):
+def score(predicted_scores, y_target, threshold):
     """ Compute metrics on classifier predictions
 
     Parameters
     ----------
-    p_train : np.array [n_samples]
-        predicted probabilities for training set
-    p_test : np.array [n_samples]
-        predicted probabilities for testing set
-    y_train : np.array [n_samples]
-        Training labels.
-    y_test : np.array [n_samples]
-        Testing labels.
+    predicted_scores : np.array [n_samples]
+        predicted scores
+    y_target : np.array [n_samples]
+        Target class labels
+    threshold : float
+        Value which determines the positive class
+        (e.g. score >= threshold --> positive class,
+         score < threshold --> negative class)
 
     Returns
     -------
-    clf_scores : dict
-        classifier scores for training set
+    scores : dict
+        dictionary of scores for the following metrics:
+            - accuracy
+            - matthews correlation coefficient
+            - precision
+            - recall
+            - f1
+            - support
+            - confusion matrix
+            - auc score
     """
-    y_pred_train = 1*(p_train >= 0.5)
-    y_pred_test = 1*(p_test >= 0.5)
+    y_predicted = 1*(predicted_scores >= threshold)
+    scores = {}
 
-    train_scores = {}
-    test_scores = {}
+    scores['accuracy'] = metrics.accuracy_score(y_target, y_predicted)
+    scores['mcc'] = metrics.matthews_corrcoef(y_target, y_predicted)
+    (scores['precision'],
+     scores['recall'],
+     scores['f1'],
+     scores['support']
+    ) = metrics.precision_recall_fscore_support(
+        y_target, y_predicted
+    )
 
-    train_scores['accuracy'] = metrics.accuracy_score(y_train, y_pred_train)
-    test_scores['accuracy'] = metrics.accuracy_score(y_test, y_pred_test)
+    scores['confusion matrix'] = metrics.confusion_matrix(
+        y_target, y_predicted, labels=[0, 1]
+    )
 
-    train_scores['mcc'] = metrics.matthews_corrcoef(y_train, y_pred_train)
-    test_scores['mcc'] = metrics.matthews_corrcoef(y_test, y_pred_test)
-
-    (p, r, f, s) = metrics.precision_recall_fscore_support(y_train,
-                                                           y_pred_train)
-    train_scores['precision'] = p
-    train_scores['recall'] = r
-    train_scores['f1'] = f
-    train_scores['support'] = s
-
-    (p, r, f, s) = metrics.precision_recall_fscore_support(y_test,
-                                                           y_pred_test)
-    test_scores['precision'] = p
-    test_scores['recall'] = r
-    test_scores['f1'] = f
-    test_scores['support'] = s
-
-    train_scores['confusion matrix'] = \
-        metrics.confusion_matrix(y_train, y_pred_train, labels=[0, 1])
-    test_scores['confusion matrix'] = \
-        metrics.confusion_matrix(y_test, y_pred_test, labels=[0, 1])
-
-    train_scores['auc score'] = \
-        metrics.roc_auc_score(y_train, p_train + 1, average='weighted')
-    test_scores['auc score'] = \
-        metrics.roc_auc_score(y_test, p_test + 1, average='weighted')
-
-    clf_scores = {'train': train_scores, 'test': test_scores}
-
-    return clf_scores
+    scores['auc score'] = metrics.roc_auc_score(
+        y_target, predicted_scores + 1, average='weighted'
+    )
+    return scores
 
 
 
