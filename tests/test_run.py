1e351a0a381fe4b7503ac9db56825ccabe79f916
Rachel Bittner <rachel.bittner@nyu.edu>
2016-08-31 00:51:31 -0400

    skip test for now

--- tests/test_run.py
+++ tests/test_run.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 
 from motif import run
+from motif.contour_extractors.HLL import BINARY_AVAILABLE
 
 
 def relpath(f):
@@ -19,7 +20,7 @@ ANNOTATION_FILE = relpath('data/input_annotation.csv')
 def array_equal(array1, array2):
     return np.all(np.isclose(array1, array2))
 
-
+@unittest.skipIf(not BINARY_AVAILABLE, "hll binary not available")
 class TestProcess(unittest.TestCase):
 
     def test_process(self):
