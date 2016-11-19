35fa2f8353ef2aaef9699452e616d2c96be682bf
Rachel Bittner <rachel.bittner@nyu.edu>
2016-11-19 15:06:28 -0500

    add mock module

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,8 @@ class Mock(MagicMock):
 MOCK_MODULES = [
     'librosa', 'sox', 'numpy', 'numpy.polynomial',
     'numpy.polynomial.polynomial', 'sklearn', 'sklearn.ensemble',
-    'sklearn.grid_search', 'sklearn.utils', 'mir_eval', 'seaborn', 'scipy',
+    'sklearn.grid_search', 'sklearn.utils', 'mir_eval', 'mir_eval.melody',
+    'mir_eval.melody.hz2cents' 'seaborn', 'scipy',
     'scipy.stats', 'matplotlib', 'matplotlib.pyplot'
 ]
 
